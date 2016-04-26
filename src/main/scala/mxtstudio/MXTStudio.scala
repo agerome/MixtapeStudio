6a9b0f192abc915981f37bcf09036196d5b15be2
agerome <alex.gerome@utexas.edu>
2016-04-26 05:02:49 -0500

    DSL scala file created

---
+++ src/main/scala/mxtstudio/MXTStudio.scala
@@ -0,0 +1,20 @@
+/**
+ * MixtapeStudio
+ * 
+ * A Scala DSL
+ *
+ * CS345, Assignment 5, Spring 2016
+ *
+ * Alex Gerome, Eric Chao
+ *
+ *
+ **/
+package mxtstudio{
+
+    import scala.collection.mutable
+
+    class MXTStudio{
+        abstract sealed class MXTLine
+
+    }
+}
